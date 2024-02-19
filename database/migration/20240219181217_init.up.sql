b4403f54286a3ab23d5ed2c2643d937b74f08178
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-02-20 02:18:19 +0800

    database migration

---
+++ database/migration/20240219181217_init.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
